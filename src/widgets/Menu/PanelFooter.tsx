cf462fd7bb86ee270f9110ccff561bdc202c74b2
ape-swap <ape.swap@outlook.com>
2021-02-13 20:07:17 -0300

    chore: update token link

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -78,7 +78,7 @@ const PanelFooter: React.FC<Props> = ({
     <Container>
       <SocialEntry>
         {cakePriceUsd ? (
-          <PriceLink href="https://pancakeswap.info/token/0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82" target="_blank">
+          <PriceLink href="https://bscscan.com/token/0x603c7f932ed1fc6575303d8fb018fdcbb0f39a95" target="_blank">
             <PancakeRoundIcon width="24px" mr="8px" />
             <Text color="textSubtle" bold>{`$${cakePriceUsd.toFixed(3)}`}</Text>
           </PriceLink>
