56569356328eb491e7fc5995c3a1e00e8aa55516
ape-swap <ape.swap@outlook.com>
2021-03-29 19:02:46 -0300

    chore: Update connectModal link

--- src/widgets/WalletModal/ConnectModal.tsx
+++ src/widgets/WalletModal/ConnectModal.tsx
@@ -31,7 +31,7 @@ const ConnectModal: React.FC<Props> = ({ login, onDismiss = () => null }) => (
       />
     ))}
     <HelpLink
-      href="https://docs.pancakeswap.finance/guides/faq#how-do-i-set-up-my-wallet-on-binance-smart-chain"
+      href="https://docs.binance.org/smart-chain/wallet/metamask.html"
       external
     >
       <HelpIcon color="primary" mr="6px" />
