263404d37370dd8b2deec8c87174d5128693cf17
Harambe-Nakamoto <haramben@protonmail.com>
2022-05-04 18:42:25 -0500

    chore: Remove language selector

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -72,13 +72,20 @@ const Footer: React.FC<FooterProps> = ({
             <div style={{ margin: "0px 12.5px" }} />
             <NetworkButton chainId={chainId} switchNetwork={switchNetwork} t={t} />
             <div style={{ margin: "0px 12.5px" }} />
+
+            {/* 
+
+            Remove until language release
+
             <LangSelector
               currentLang={currentLang}
               langs={langs}
               setLang={setLang}
               color={darkColors.text as keyof Colors}
               dropdownPosition="top-right"
-            />
+            /> 
+            
+            */}
           </ButtonFlex>
           <IconFlex>
             <StyledLink href="https://twitter.com/ape_swap" target="_blank" rel="noopener noreferrer">
--- src/widgets/Navbar/MenuLink.tsx
+++ src/widgets/Navbar/MenuLink.tsx
@@ -1,11 +1,11 @@
 import React, { AnchorHTMLAttributes } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 const MenuLink: React.FC<AnchorHTMLAttributes<HTMLAnchorElement>> = ({ href, ...otherProps }) => {
   const isHttpLink = href?.startsWith("http");
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const Tag: any = isHttpLink ? "a" : NavLink;
+  const Tag: any = isHttpLink ? "a" : Link;
   const props = isHttpLink ? { href } : { to: href };
   return <Tag {...props} {...otherProps} />;
 };
