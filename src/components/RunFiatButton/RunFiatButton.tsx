0e53131d6c0a055f7841e4ca0ef3036525d248a5
Baba Drape <babadrape@gmail.com>
2022-05-05 02:18:25 +0100

    last change

--- src/components/RunFiatButton/RunFiatButton.tsx
+++ src/components/RunFiatButton/RunFiatButton.tsx
@@ -33,7 +33,7 @@ const RunFiatButton: React.FC<RFBProps> = ({ runFiat, mini, ...props }) => {
           <CardIcon fill="#FAFAFA" marginLeft="5px" />
         </>
       )}
-      {mini && <CardIcon fill={(theme.isDark && "#FAFAFA") || "#4D4040"} />}
+      {mini && <CardIcon fill={theme.isDark ? "#FAFAFA" : "#4D4040"} />}
     </Button>
   );
 };
