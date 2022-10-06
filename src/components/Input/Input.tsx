fdf1a7eddb47459aba2555104443600309af3767
Baba Drape <babadrape@gmail.com>
2022-10-06 07:28:25 +0100

    add message icon and modify input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -3,7 +3,7 @@ import { Input as ThemeUIInput, Box } from "theme-ui";
 import { InputProps, sizes, iconSizes } from "./types";
 import { Svg } from "../Svg";
 
-const Input: React.FC<InputProps> = ({ size = sizes.MD, icon, width, sx, ...props }) => {
+const Input: React.FC<InputProps> = ({ size = sizes.MD, icon, width, ...props }) => {
   return (
     <Box
       sx={{
@@ -16,7 +16,7 @@ const Input: React.FC<InputProps> = ({ size = sizes.MD, icon, width, sx, ...prop
         sx={{
           width,
           pl: "15px",
-          pr: icon ? "40px" : "15px",
+          pr: icon ? "40px" : width === '100%' ? 0 : "15px",
           variant: `forms.input.${size}`,
           color: "text",
           outline: "none",
