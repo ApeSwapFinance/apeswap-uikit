8af4da89d56523b600926b12250fc5ba4fddd794
Ape Fede <apefede09@gmail.com>
2022-10-17 13:28:05 -0500

    chore: Select network update

--- src/widgets/NetworkModal/SelectNetworkModal.tsx
+++ src/widgets/NetworkModal/SelectNetworkModal.tsx
@@ -4,6 +4,9 @@ import { SwitchNetwork } from "./types";
 import { Heading } from "../../components/Heading";
 import networks from "./config";
 import NetworkCard from "./NetworkCard";
+import { Svg } from "../../components/Svg";
+import { Flex } from "../../components/Flex";
+import styles from "./styles";
 
 interface Props {
   switchNetwork: SwitchNetwork;
@@ -20,6 +23,19 @@ const SelectNetworkModal: React.FC<Props> = ({ switchNetwork, chainId, t, onDism
     {networks.map((network) => (
       <NetworkCard networkConfig={network} chainId={chainId} switchNetwork={switchNetwork} key={network.chainId} />
     ))}
+    <Flex sx={styles.bridge}>
+      <a
+        href="https://app.multichain.org/#/router"
+        target="_blank"
+        rel="noopener noreferrer"
+        style={{
+          marginRight: "3px",
+        }}
+      >
+        {t("Bridge Tokens")}
+      </a>
+      <Svg icon="caret" width="6px" direction="right" />
+    </Flex>
   </Modal>
 );
 
