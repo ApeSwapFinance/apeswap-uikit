48643dd23e1b55532d599d47ecafb174431066aa
Ape Fede <75544302+ape-fede@users.noreply.github.com>
2023-02-24 16:00:11 -0300

    chore: Added test

--- src/__tests__/widgets/walletmodal.test.tsx
+++ src/__tests__/widgets/walletmodal.test.tsx
@@ -467,11 +467,8 @@ it("renders ConnectModal correctly", () => {
                     class="css-mjyeuz-WalletCard"
                   >
                     <svg
-                      class="sc-bdfBQB kA-DbXw"
-                      color="text"
                       viewBox="0 0 96 96"
                       width="100%"
-                      xmlns="http://www.w3.org/2000/svg"
                     >
                       <circle
                         cx="48"
@@ -614,11 +611,8 @@ it("renders ConnectModal correctly", () => {
                     class="css-mjyeuz-WalletCard"
                   >
                     <svg
-                      class="sc-bdfBQB kA-DbXw"
-                      color="text"
                       viewBox="0 0 96 96"
                       width="100%"
-                      xmlns="http://www.w3.org/2000/svg"
                     >
                       <g
                         clip-path="url(#clip0)"
@@ -662,11 +656,8 @@ it("renders ConnectModal correctly", () => {
                     class="css-mjyeuz-WalletCard"
                   >
                     <svg
-                      class="sc-bdfBQB kA-DbXw"
-                      color="text"
                       viewBox="0 0 96 96"
                       width="100%"
-                      xmlns="http://www.w3.org/2000/svg"
                     >
                       <path
                         d="M96 48C96 21.4903 74.5097 0 48 0C21.4903 0 0 21.4903 0 48C0 74.5097 21.4903 96 48 96C74.5097 96 96 74.5097 96 48Z"
