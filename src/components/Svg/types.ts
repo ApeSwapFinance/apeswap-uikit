833dc6b0a6f6555735b5160ba86e39fbfe390b88
Ape Fede <75544302+ape-fede@users.noreply.github.com>
2023-02-24 16:28:02 -0300

    chore: Added back LegacySvgProps

--- src/components/Svg/types.ts
+++ src/components/Svg/types.ts
@@ -1,3 +1,6 @@
+import { SVGAttributes } from "react";
+import { DefaultTheme } from "styled-components";
+import { SpaceProps } from "styled-system";
 import { colorProps } from "../../theme/Apeswap/types";
 
 export enum icons {
@@ -77,6 +80,11 @@ export enum directions {
 export type iconTypes = `${icons}`;
 type directionTypes = `${directions}`;
 
+export interface LegacySvgProps extends SVGAttributes<HTMLOrSVGElement>, SpaceProps {
+  theme?: DefaultTheme;
+  spin?: boolean;
+  bgColor?: string;
+}
 export interface SvgProps {
   icon?: iconTypes;
   direction?: directionTypes;
