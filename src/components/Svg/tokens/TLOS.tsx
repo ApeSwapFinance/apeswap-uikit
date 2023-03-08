b4f23e05bb7b27618a6284d1ff63973786c99438
Ape Fede <75544302+ape-fede@users.noreply.github.com>
2023-03-08 17:34:08 -0300

    chore: White circle to chainIcons

--- src/components/Svg/tokens/TLOS.tsx
+++ src/components/Svg/tokens/TLOS.tsx
@@ -3,32 +3,41 @@ import { SvgProps } from "../types";
 
 const TLOS: React.FC<SvgProps> = (props) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none" {...props}>
-      <g clipPath="url(#clip0_1295_45629)">
+    <svg width="30" height="31" viewBox="0 0 30 31" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g clipPath="url(#clip0_286_7965)">
         <path
-          d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z"
-          fill="url(#paint0_linear_1295_45629)"
+          d="M15 30.5C23.2843 30.5 30 23.7843 30 15.5C30 7.21573 23.2843 0.5 15 0.5C6.71573 0.5 0 7.21573 0 15.5C0 23.7843 6.71573 30.5 15 30.5Z"
+          fill="url(#paint0_linear_286_7965)"
         />
-        <g filter="url(#filter0_i_1295_45629)">
+        <g filter="url(#filter0_i_286_7965)">
           <path
-            d="M16 32C24.8366 32 32 24.8366 32 16C32 7.16344 24.8366 0 16 0C7.16344 0 0 7.16344 0 16C0 24.8366 7.16344 32 16 32Z"
-            fill="url(#paint1_linear_1295_45629)"
+            d="M29.5 15.5C29.5 23.5081 23.0081 30 15 30C6.99187 30 0.5 23.5081 0.5 15.5C0.5 7.49187 6.99187 1 15 1C23.0081 1 29.5 7.49187 29.5 15.5Z"
+            fill="url(#paint1_linear_286_7965)"
+            stroke="#FAFAFA"
           />
         </g>
-        <g filter="url(#filter1_di_1295_45629)">
+        <g filter="url(#filter1_di_286_7965)">
           <path
-            d="M11.3 25.5C10.5 25.5 9.8 25 9.5 24.2C9.4 23.9 9.3 23.5 9.2 23.1C8.6 21 7.8 18.5 8 16.2C8 15.4 8.2 14.7 8.6 14L8 12.3C7.7 11.5 8 10.6 8.6 10L10.2 8.60004C10.6 8.30004 11.1 8.10004 11.6 8.10004H16.8L17.4 7.20004C17.7 6.80004 18.1 6.60004 18.5 6.70004L21.7 6.90004C22.1 6.90004 22.5 7.10004 22.7 7.50004C22.9 7.90004 23 8.30004 22.8 8.60004L22 11V11.1L24.6 15.6C25.1 16.3 24.9 17.3 24.6 18.3C24.5 18.6 24.5 18.8 24.4 19.1C24.2 20 23.6 20.7 22.7 20.8L21 21.1C20.6 21.8 19.8 22.5 18.9 23C17.4 24.1 15.2 24.6 13.3 25C12.7 25.1 12.2 25.3 11.7 25.4C11.6 25.5 11.5 25.5 11.3 25.5ZM11.6 10.1L10 11.5C10 11.5 9.9 11.6 10 11.6L10.9 14.2L10.6 14.6C10.3 15 10 15.6 10 16.2V16.3V16.4C9.8 18.3 10.5 20.7 11.1 22.6C11.2 22.9 11.3 23.2 11.4 23.6C11.9 23.5 12.4 23.4 12.9 23.2C14.6 22.8 16.6 22.4 17.9 21.5C18.7 21 19.3 20.5 19.5 20L19.7 19.5L22.4 19C22.4 19 22.5 19 22.5 18.9C22.5 18.7 22.6 18.4 22.7 18C22.8 17.7 22.9 17 22.9 16.8L20.3 12.3C20 11.7 19.9 11.1 20.2 10.5L20.8 9.00004L19 8.90004L18.4 9.80004C18.2 10.1 17.8 10.4 17.3 10.4L11.6 10.1Z"
-            fill="white"
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M20.5764 8.27205C20.64 8.10652 20.5251 7.92673 20.3482 7.915L17.3647 7.71713C17.2725 7.71102 17.1838 7.75373 17.1311 7.82966L16.3406 8.96842C16.2911 9.03971 16.2096 9.08202 16.1229 9.08152L10.871 9.05052C10.6134 9.049 10.3641 9.14205 10.1704 9.31202L8.71327 10.591C8.38906 10.8756 8.2713 11.3292 8.41611 11.7355L9.11019 13.6829C9.11374 13.6929 9.11206 13.7041 9.10564 13.7126C8.71567 14.231 8.48896 14.8538 8.45438 15.5016L8.44261 15.7222C8.36917 17.0987 8.53125 18.4773 8.92204 19.7992C9.24406 20.8885 9.61311 22.1369 9.83932 22.9021C9.95867 23.3058 10.371 23.544 10.7804 23.4457C11.6617 23.234 13.1754 22.8704 14.3675 22.5834C15.4157 22.3309 16.4125 21.9006 17.3162 21.3125L17.8817 20.9445C18.4255 20.5907 18.8515 20.0829 19.1054 19.4859C19.1096 19.4762 19.1184 19.4691 19.1289 19.4672L21.1624 19.0946C21.5867 19.0168 21.9206 18.688 22.005 18.265L22.384 16.3635C22.4344 16.1108 22.3904 15.8484 22.2602 15.6261L19.7931 11.4106C19.6315 11.1345 19.6043 10.7998 19.7191 10.5011L20.5764 8.27205Z"
+            stroke="white"
+            strokeWidth="2"
           />
         </g>
+        <path
+          d="M29 15.5C29 23.232 22.732 29.5 15 29.5C7.26801 29.5 1 23.232 1 15.5C1 7.76801 7.26801 1.5 15 1.5C22.732 1.5 29 7.76801 29 15.5Z"
+          stroke="#FAFAFA"
+          strokeWidth="2"
+        />
       </g>
       <defs>
         <filter
-          id="filter0_i_1295_45629"
+          id="filter0_i_286_7965"
           x="-9.5"
-          y="0"
-          width="41.5"
-          height="32"
+          y="0.5"
+          width="39.5"
+          height="30"
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
@@ -44,14 +53,14 @@ const TLOS: React.FC<SvgProps> = (props) => {
           <feGaussianBlur stdDeviation="4.75" />
           <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
           <feColorMatrix type="matrix" values="0 0 0 0 0.682353 0 0 0 0 0.45098 0 0 0 0 0.980392 0 0 0 0.56 0" />
-          <feBlend mode="normal" in2="shape" result="effect1_innerShadow_1295_45629" />
+          <feBlend mode="normal" in2="shape" result="effect1_innerShadow_286_7965" />
         </filter>
         <filter
-          id="filter1_di_1295_45629"
-          x="6.92133"
-          y="6.19407"
-          width="18.9442"
-          height="20.7475"
+          id="filter1_di_286_7965"
+          x="6.394"
+          y="6.23606"
+          width="17.9713"
+          height="19.6734"
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
         >
@@ -65,8 +74,8 @@ const TLOS: React.FC<SvgProps> = (props) => {
           <feOffset dy="0.480491" />
           <feGaussianBlur stdDeviation="0.480491" />
           <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0" />
-          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1295_45629" />
-          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1295_45629" result="shape" />
+          <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_286_7965" />
+          <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_286_7965" result="shape" />
           <feColorMatrix
             in="SourceAlpha"
             type="matrix"
@@ -77,18 +86,18 @@ const TLOS: React.FC<SvgProps> = (props) => {
           <feGaussianBlur stdDeviation="0.480491" />
           <feComposite in2="hardAlpha" operator="arithmetic" k2="-1" k3="1" />
           <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.12 0" />
-          <feBlend mode="normal" in2="shape" result="effect2_innerShadow_1295_45629" />
+          <feBlend mode="normal" in2="shape" result="effect2_innerShadow_286_7965" />
         </filter>
-        <linearGradient id="paint0_linear_1295_45629" x1="16" y1="0" x2="16" y2="32" gradientUnits="userSpaceOnUse">
+        <linearGradient id="paint0_linear_286_7965" x1="15" y1="0.5" x2="15" y2="30.5" gradientUnits="userSpaceOnUse">
           <stop stopColor="#130C3F" />
           <stop offset="1" stopColor="#15197B" />
         </linearGradient>
-        <linearGradient id="paint1_linear_1295_45629" x1="0" y1="16" x2="32" y2="16" gradientUnits="userSpaceOnUse">
+        <linearGradient id="paint1_linear_286_7965" x1="0" y1="15.5" x2="30" y2="15.5" gradientUnits="userSpaceOnUse">
           <stop stopColor="#AE73FA" />
           <stop offset="1" stopColor="#AE73FA" stopOpacity="0" />
         </linearGradient>
-        <clipPath id="clip0_1295_45629">
-          <rect width="32" height="32" fill="white" />
+        <clipPath id="clip0_286_7965">
+          <rect width="30" height="30" fill="white" transform="translate(0 0.5)" />
         </clipPath>
       </defs>
     </svg>
