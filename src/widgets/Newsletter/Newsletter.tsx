ba2793dae9eee45d93baec2a2a278b43e289c320
Obie Dobo <obiedobo@gmail.com>
2023-04-01 08:18:40 -0500

    chore: Mailjet API (dummy key)

--- src/widgets/Newsletter/Newsletter.tsx
+++ src/widgets/Newsletter/Newsletter.tsx
@@ -1,21 +1,42 @@
 import React from "react";
-import MailChimpSubscribe from "react-mailchimp-subscribe";
+import mailjet from "node-mailjet";
 import NewsletterForm from "./NewsletterForm";
 import { NewsletterProps } from "./types";
 
-const Newsletter: React.FC<NewsletterProps> = ({ isNewsModal, mailChimpUrl, t }) => {
+const Newsletter: React.FC<NewsletterProps> = ({ isNewsModal, t }) => {
+  const [status, setStatus] = React.useState("");
+  const [message, setMessage] = React.useState("");
+
+  // TODO: Move to env variables
+  const [mailjetApiKey, mailjetSecretKey] = ["bc02fa7032916da1142a484e6d92f4c5", "7ea011bcb5783bb4ec94822eda5cf341"];
+
+  const subscribe = async (formData: { [x: string]: any }) => {
+    const client = mailjet.apiConnect(mailjetApiKey, mailjetSecretKey);
+
+    console.log(formData.EMAIL)
+
+    try {
+      const contact = await client.post("contact", { version: "v3" }).request({
+        Email: formData.EMAIL,
+      });
+
+      console.log(contact);
+
+      setStatus("success");
+      setMessage("Thank you for subscribing!");
+    } catch (error: any) {
+      setStatus("error");
+      setMessage(error.message);
+    }
+  };
+
   return (
-    <MailChimpSubscribe
-      url={mailChimpUrl}
-      render={({ subscribe, status, message }: any) => (
-        <NewsletterForm
-          status={status}
-          message={message}
-          onValidated={(formData) => subscribe(formData)}
-          isModal={isNewsModal}
-          t={t}
-        />
-      )}
+    <NewsletterForm
+      status={status}
+      message={message}
+      onValidated={(formData) => subscribe(formData)}
+      isModal={isNewsModal}
+      t={t}
     />
   );
 };
